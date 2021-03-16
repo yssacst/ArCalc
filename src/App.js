2eacd6b265f6612081d434695d80f9fecacdc39e
Patrick Araujo <rayssacoosta@gmail.com>
2021-03-16 14:49:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          <form>
+              <label> Horas/dia
+                <input/>
+              </label>
+              <label>Dias/mês
+                <input/>
+              </label>
+              <label>Consumo do aparelho (kW/h por mês)
+                <input/>
+              </label>
+              <label>Preço da energia (R$ por kW/h)
+                <input/>
+              </label>
+              <button type="submit">Calcular</button>
+          </form>
+          <label>
+            Resultado
+            <p>{resultado}</p>
+          </label>
+      </div>
     </div>
   );
 }
