6f257f5b285cedd984f3334824f23bd6dc6a5332
Patrick Araujo <rayssacoosta@gmail.com>
2021-03-17 14:15:07 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -40,18 +40,19 @@ function App() {
 
   return (
     <div className="container" onMouseMove={ handleCursor } style={{ backgroundPosition:`${moveX}px ${moveY}px` }}>
+      <p className="lbl-titulo">Calculadora de Consumo de Ar Condicionado</p>
       <div className="bx-principal">
         <div className="bx-aux">
-            <label className="lbl-titulo">Informe os dados</label>
+            <label className="lbl-legenda">Informe os dados</label>
             <form className="frm-calc">
-                <label htmlFor="horas"> Horas/dia</label>
-                <input type="number" step="0.1" id="horas" onChange={handleChangeHoras} required/>
+                <label htmlFor="consumo">Consumo do aparelho (kW/h por mês)</label>
+                <input type="number" step="0.1" id="consumo" onChange={handleChangeConsumo} required/>
                 
                 <label htmlFor="dias">Dias/mês</label>
                 <input type="number" step="0.1" id="dias" onChange={handleChangeDias} required/>
                 
-                <label htmlFor="consumo">Consumo do aparelho (kW/h por mês)</label>
-                <input type="number" step="0.1" id="consumo" onChange={handleChangeConsumo} required/>
+                <label htmlFor="horas"> Horas/dia</label>
+                <input type="number" step="0.1" id="horas" onChange={handleChangeHoras} required/>
                 
                 <label htmlFor="preco">Preço da energia (R$ por kW/h)</label>
                 <input type="number" step="0.1" id="preco" onChange={handleChangePreco} required/>
@@ -60,12 +61,13 @@ function App() {
             </form>
         </div>
         <div className="bx-aux">
-          <label className="lbl-titulo">Resultado</label>
+          <label className="lbl-legenda">Resultado</label>
           <div className="bx-resultado">
             { !resultado ? <p>Preencha os campos anteriores para saber o consumo</p>: 
               <label>
                 O valor aproximado de consumo é
-                <p>{resultado}</p>
+                <p className="lbl-resultado">R$ {resultado} </p>
+                Usando {horas} horas por {dias} dias
               </label>
               }
           </div>
@@ -83,6 +85,7 @@ function App() {
           <p className="txt-explicacao"><strong>Preço do kW/h</strong>: Nesse campo você deverá informar <strong>quanto custa o kW/h na sua região</strong>. Você consegue encontrar essa informação na sua conta de luz.</p>
         </div>
       </div>
+      <span className="footer">Desenvolvido por Rayssa Costa - 2021</span>
     </div>
   );
 }
